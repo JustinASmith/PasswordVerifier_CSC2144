a9c7e3568d16fd50f491a59d1d45fda44e16b62b
jasmith17 <jasmith17@iccms.edu>
2019-03-08 13:13:28 -0600

    Updated driver cpp file with program description and skeleton.

---
+++ PasswordVerifier_CSC2144/PasswordVerifier_CSC2144/PasswordVerifier_CSC2144.cpp
@@ -0,0 +1,19 @@
+// Justin Allen Smith
+// CSC 2144 - Spring 2019
+// 8, March 2019
+
+// This program asks a user to input a new password
+// then verifies that it meets the required criteria
+// if it does meet criteria, program displays a success message
+// if it does not meet criteria, program displays a message
+// identifying what is wrong with the criteria
+
+#include <iostream>
+
+int main(void)
+{
+
+
+	system("PAUSE");
+	return 0;
+}
